e6dcd49d18b3c12ba518925658452410b64d40c4
Victor <vclarindo@ciandt.com>
2025-07-27 19:27:32 -0300

    Some improvements in the forms

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [
+    FlexLayoutModule, 
+    FormsModule, 
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
